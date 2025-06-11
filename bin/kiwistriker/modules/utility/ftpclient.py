310c22e3a0cba4504b1aa64f639068d6f4b6c657
Alex <alexedwardjames09@gmail.com>
2025-06-11 13:41:51 +0930

    Reworked bruteclaw

--- bin/kiwistriker/modules/utility/ftpclient.py
+++ bin/kiwistriker/modules/utility/ftpclient.py
@@ -1,1 +1,9 @@
+import ftplib
+
 #!/usr/bin/python3
+# Create and return a logged in FTP object
+def connectFTP(targetIP, username, password):
+    ftp = ftplib.FTP()
+
+    ftp.connect(targetIP, 21)
+    ftp.login(username, password)
