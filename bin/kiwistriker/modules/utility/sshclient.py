aab7ab7c2de5ff2eb05748cf861db134a0d4f639
Alex <alexedwardjames09@gmail.com>
2025-06-13 08:22:46 +0930

    Server client development.

--- bin/kiwistriker/modules/utility/sshclient.py
+++ bin/kiwistriker/modules/utility/sshclient.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+import paramiko
+
+# Connect to SSH server and return connected object
+def connectSSH(targetIP, username, password):
+	try:
+		ssh = paramiko.SSHClient()
+		ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+		ssh.connect(hostname=targetIP, port=22, username=username, password=password, timeout=5)
+
+		return ssh
+
+	except Exception as e:
+		print(f"[-] Error: {e}")
+
+def sshClientLoop(targetIP, username, password)
