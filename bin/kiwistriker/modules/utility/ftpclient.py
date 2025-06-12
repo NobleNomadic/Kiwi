768acef27e144f8ef0e86aae0f105bcc75d030e9
nomad <nomad@kiwi.home>
2025-06-12 18:54:31 +0930

    Started FTP client foundations

--- bin/kiwistriker/modules/utility/ftpclient.py
+++ bin/kiwistriker/modules/utility/ftpclient.py
@@ -3,7 +3,26 @@ import ftplib
 #!/usr/bin/python3
 # Create and return a logged in FTP object
 def connectFTP(targetIP, username, password):
-    ftp = ftplib.FTP()
+    try:
+        ftp = ftplib.FTP()
 
-    ftp.connect(targetIP, 21)
-    ftp.login(username, password)
+        ftp.connect(targetIP, 21)
+        ftp.login(username, password)
+
+        print("[+] Logged into FTP")
+        return ftp
+
+    except:
+        print(f"[-] Error while connecting to FTP: {e}")
+
+
+def FTPClientLoop(targetIP, username, password):
+    ftp = connectFTP(targetIP, username, password)
+
+    while True:
+        command = input(f"FTP@{targetIP}> ")
+        if command.lower() == "exit":
+            break
+
+    ftp.quit()
+    return
